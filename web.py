5d599c5f46ea98b508123c2ecdc84c9fce28d9ba
ho94949 <sakura@kyouko.moe>
2015-06-04 11:16:44 +0900

    Index.html Hello, {{username}} 추가, debug=true

--- web.py
+++ web.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
-from flask import Flask
+from flask import Flask, request, make_response, redirect, render_template
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-	return 'test'
+	return render_template('index.html',username='HYEA')
 
 @app.route('/static/<filename>')
 def static_files(filename):
@@ -12,4 +12,4 @@ def static_files(filename):
 	return f.read()
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
