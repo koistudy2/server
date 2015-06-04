45ea706a1bbc2b876dd3b5a7b8b833f2a508c853
hletrd <01@0101010101.com>
2015-06-04 10:56:56 +0900

    Initial Commit

---
+++ web.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+	return 'test'
+
+@app.route('/static/<filename>')
+def static(filename):
+	f = open('./static/' + filename)
+	return f.read()
+
+if __name__ == '__main__':
+    app.run()
