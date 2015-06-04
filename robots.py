6f1fbb1bb05daae4d6508c72d986a994af3d3171
hletrd <01@0101010101.com>
2015-06-05 00:03:23 +0900

    humans.txt

--- robots.py
+++ robots.py
@@ -3,3 +3,11 @@
 @app.route('/robots.txt')
 def robots():
 	return 'User-agent: *\r\nAllow: /'
+
+@app.route('/humans.txt')
+def humans():
+	return """User-agent: *
+Allow: /
+Disallow: /admin
+User-agent: Administrator
+Allow: /admin"""
