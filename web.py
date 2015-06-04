13a7b6ac274dd3b81df5bd2a92d59e54784d8ad6
hletrd <01@0101010101.com>
2015-06-04 20:26:41 +0900

    Added Recaptcha

--- web.py
+++ web.py
@@ -13,8 +13,8 @@ sys.setdefaultencoding('utf-8')
 #session setup
 app.secret_key = open("/dev/random","rb").read(32)
 
-def render(title, content):
-	return render_template('basic_template.html', title=title, content=content, lang=lang.lang[session.get('locale', 'ko')], menus=configs.menus)
+def render(title='KOISTUDYS2', content='', mode=''):
+	return render_template('basic_template.html', title=title, content=content, lang=lang.lang[session.get('locale', 'ko')], menus=configs.menus, session=session, mode=mode)
 
 @app.before_request
 def initApp():
@@ -26,6 +26,11 @@ def initApp():
 def index():
 	content = ''
 	return render("KOISTUDYS2", "Sample Content")
+
+@app.route('/signup')
+def signup():
+	content = ''
+	return render('KOISTUDYS2', content, 'signup')
 
 @app.route('/static/<path:path>')
 def static_files(path):
