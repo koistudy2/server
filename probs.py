4a562c64bbeea80e275ae558b4a1fe07cc51b035
Namnamseo <0201ssw+git@gmail.com>
2015-06-06 02:15:54 +0900

    Fixed problem page. Issue needed.

--- probs.py
+++ probs.py
@@ -2,8 +2,9 @@
 
 @app.route('/probs')
 def probs():
-	problist = {}
-	problist[0] = {'num': '1', 'verdict': 'AC', 'name': 'Hello World', 'solved' : '1', 'tried' : '1', 'diff' : '1'};
-	problist[1] = {'num': '2', 'verdict': 'WA', 'name': 'Goodbye Cruel World', 'solved' : '4', 'tried' : '4444', 'diff' : '9'};
+	submit_list = [ {'num': '1', 'verdict': 'AC', 'name': 'Hello World', 'solved' : '1', 'tried' : '1', 'diff' : '1'},
+	             {'num': '2', 'verdict': 'WA', 'name': 'Goodbye Cruel World', 'solved' : '4', 'tried' : '4444', 'diff' : '9'} ];
+	key_list = [('num','Number'), ('verdict','Verdict'), ('name','Name'),
+		('solved','Solved'), ('tried','Tried'), ('diff','Diff')]
 
-	return renderprob(problist,'KOISTUDYS2', '', 'probs')
+	return newrender('title_probs', '', 'probs.html', 'probs', {'submit_list':submit_list, 'key_list':key_list})
