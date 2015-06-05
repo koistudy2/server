5e0ecbf80ac4698cf9e8578eb6941cf960259250
hletrd <01@0101010101.com>
2015-06-05 22:52:18 +0900

    Basic user info page

--- user.py
+++ user.py
@@ -2,4 +2,12 @@
 
 @app.route('/user')
 def user():
-	return newrender('title_user', '', 'user.html')
+	import dbhandler
+	try:
+		user = dbhandler.col_members.find_one({"username": session['username']})
+		data = {}
+		data['name'] = user['name']
+		data['username'] = user['username']
+		return newrender('title_user', '', 'user.html', '', data)
+	except KeyError:
+		return newrender('title_error', 'Error')
