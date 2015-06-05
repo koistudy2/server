1d8634778e13dc42a7bef45bd450218e4b290ce1
hletrd <01@0101010101.com>
2015-06-05 12:19:35 +0900

    incorrectly escaped

--- signup.py
+++ signup.py
@@ -1,5 +1,10 @@
 # -*- coding: utf-8 -*-
 #Logic Related to Signup
+
+import sys #to supress unicodedecodeerror
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
 @app.route('/signup')
 def signup():
 	return render('KOISTUDYS2', '', 'signup')
@@ -16,11 +21,11 @@ def signup_submit():
 	if not result['success'] and 1==21:
 		return render('KOISTUDYS2', '', 'signup_err_captcha')
 	else:
-		if re.match('^[a-zA-Zㄱ-ㅎㅏ-ㅣ가-힣0-9_\-\.]{4,20}$', request.form['username']):
+		if re.match('^[a-zA-Z가-힣ㄱ-ㅎㅏ-ㅣ0-9_\\-.]{4,20}$', request.form['username']):
 			if re.match('^.{6,200}$', request.form['password']):
 				if request.form['password'] == request.form['password_re']:
 					if re.match('^[가-힣A-Za-z ]{2,30}$', request.form['name']):
-						if re.match('^[a-zA-Z._+-0-9]+@[a-z0-9.-]+\.[a-z]{2,5}$', request.form['email']):
+						if re.match('^[a-zA-Z._+-0-9]+@[a-z0-9.-]+\\.[a-z]{2,5}$', request.form['email']):
 							return render('KOISTUDYS2', 'Signup completed', 'signup_complete')
 						else:
 							return render('KOISTUDYS2', '', 'signup_err_email')
@@ -29,7 +34,7 @@ def signup_submit():
 				else:
 					return render('KOISTUDYS2', '', 'signup_err_pwmatch')
 			else:
-				return render('KOISTUDYS2', '', 'signup_err_username')
+				return render('KOISTUDYS2', '', 'signup_err_password')
 		else:
 			return render('KOISTUDYS2', '', 'signup_err_username')
 
