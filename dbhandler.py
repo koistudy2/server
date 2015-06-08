2b715fb9871933b82f606c2b8d49af6fb3944773
hletrd <01@0101010101.com>
2015-06-09 02:38:00 +0900

    Submit

--- dbhandler.py
+++ dbhandler.py
@@ -5,7 +5,8 @@ dbclient = MongoClient('localhost', 27017)
 db = dbclient.db_main
 
 col_members = db.members
-col_logs    = db.logs
-col_probs   = db.probs
-col_contests   = db.contests
-col_descs   = db.descs
+col_logs = db.logs
+col_probs = db.probs
+col_contests = db.contests
+col_descs = db.descs
+col_judges = db.judges
--- submit.py
+++ submit.py
@@ -1,4 +1,5 @@
 from flask import request
+import dbhandler
 
 def submit(probid):
-	return ''
+	return newrender('')
