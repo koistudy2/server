019b23760a533cbeb28037ceff218cf76603bf4d
ho94949 <sakura@kyouko.moe>
2015-06-09 13:41:00 +0900

    HYEA  added

--- configs.py
+++ configs.py
@@ -13,7 +13,7 @@ t_prefix = 'KOISTUDYS2' #title prefix
 default_url = 'https://koistudy2.0101010101.com' #no slash, please
 
 probs_per_page = 20
-admin = ['hletrd', 'hletrd002', 'Namnamseo', 'koosaga']
+admin = ['hletrd', 'hletrd002', 'Namnamseo', 'koosaga', 'HYEA']
 langs = ['ko', 'en']
 
 execfile('secretconfig.py')
