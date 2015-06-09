725960ad2c5b1de07c64637b29b5916f9782155e
hletrd <01@0101010101.com>
2015-06-09 19:08:01 +0900

    send_from_directory

--- static.py
+++ static.py
@@ -1,12 +1,14 @@
 #Logic Related to static files
 
+from flask import send_from_directory
 from error_handler import error_404
 
 def static_files(path):
-	if '.' in path:
-		return error_404(0)
-	if os.path.isfile(s_prefix + path):
-		f = open(s_prefix + path)
-		return f.read()
-	else:
-		return error_404(0)
+	return send_from_directory(app.config['UPLOAD_FOLDER'], path, as_attachment=False)
+	#if '.' in path:
+	#	return error_404(0)
+	#if os.path.isfile(s_prefix + path):
+	#	f = open(s_prefix + path)
+	#	return f.read()
+	#else:
+	#	return error_404(0)
