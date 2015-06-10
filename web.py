12b5e2cb3407118b74d3d7468dfcf57d2b03e60e
Namnamseo <0201ssw+github@gmail.com>
2015-06-10 13:57:47 +0900

    secret_key uses random module
    On most Linux systems, /dev/random is powered from actual entropy gathered by the environment. If your system isn't delivering a large amount of data from /dev/random, it likely means that you're not generating enough environmental randomness to power it.

--- web.py
+++ web.py
@@ -19,12 +19,7 @@ reload(sys)
 sys.setdefaultencoding('utf-8')
 
 #session setup
-if os.path.exists("/dev/random"):
-	app.secret_key = open("/dev/random","rb").read(32)
-elif os.path.exists("/dev/urandom"):
-	app.secret_key = open("/dev/urandom","rb").read(32)
-else:
-	app.secret_key = ''.join(random.choice(string.ascii_uppercase) for _ in range(32))
+app.secret_key = ''.join(random.choice(string.ascii_letters+string.digits) for _ in range(32))
 
 @app.before_request
 def initApp():
