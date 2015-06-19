07b2fd92cf8980bbccc9b107c1926cb87b26ac3a
hletrd <01@0101010101.com>
2015-06-19 11:18:11 +0900

    locale for prob list

--- probs.py
+++ probs.py
@@ -5,26 +5,28 @@ from functional import newrender
 import configs
 import dbhandler
 import math
+import lang
 
 def probs(page=1):
 	page -= 1
-	key_list = [('unique_id','Code'), ('verdict','Verdict'), ('display_name','Name'), ('solved','Solved'), ('submits','Submits'), ('diff','Diff')]
+	a = lang.lang[session.get('locale', 'ko')]
+	key_list = [('unique_id', a['probs_code']), ('verdict', a['probs_verdict']), ('display_name', a['probs_name']), ('solved', a['probs_solved']), ('submits', a['probs_submits']), ('diff',a['probs_diff'])]
 	is_admin = False
 	if 'username' in session and session['username'] in configs.admin:
-		prob_list = dbhandler.col_probs.find().skip(page * configs.probs_per_page).limit(configs.probs_per_page)
+		submit_list = dbhandler.col_probs.find().skip(page * configs.probs_per_page).limit(configs.probs_per_page)
 		probcnt = dbhandler.col_probs.count()
 		is_admin = True
 	elif 'username' in session and session['username']:
 		user = dbhandler.col_members.find_one({"username": session['username']})
 		if 'gshs' in user:
-			prob_list = dbhandler.col_probs.find({"visib": True}).skip(page * configs.probs_per_page).limit(configs.probs_per_page)
-			probcnt = dbhandler.col_probs.count({"visib": True})
+			submit_list = dbhandler.col_probs.find({"visib": False}).skip(page * configs.probs_per_page).limit(configs.probs_per_page)
+			probcnt = dbhandler.col_probs.count({"visib": False})
 		else:
-			prob_list = dbhandler.col_probs.find({"visib": True, "gshs": False}).skip(page * configs.probs_per_page).limit(configs.probs_per_page)
-			probcnt = dbhandler.col_probs.count({"visib": True, "gshs": False})
+			submit_list = dbhandler.col_probs.find({"visib": False, "gshs": False}).skip(page * configs.probs_per_page).limit(configs.probs_per_page)
+			probcnt = dbhandler.col_probs.count({"visib": False, "gshs": False})
 	else:
-		prob_list = dbhandler.col_probs.find({"visib": True, "gshs": False}).skip(page * configs.probs_per_page).limit(configs.probs_per_page)
-		probcnt = dbhandler.col_probs.count({"visib": True, "gshs": False})
+		submit_list = dbhandler.col_probs.find({"visib": False, "gshs": False}).skip(page * configs.probs_per_page).limit(configs.probs_per_page)
+		probcnt = dbhandler.col_probs.count({"visib": False, "gshs": False})
 	user = dbhandler.col_members.find({"username": session.get('username')})
 	if session.get('username') in configs.admin:
 		is_admin = True
@@ -48,4 +50,4 @@ def probs(page=1):
 		pager1 = 0
 		pager2 = 0
 		pager3 = 0
-	return newrender('title_probs', '', 'probs.html', 'probs', {'prob_list':prob_list, 'key_list':key_list, 'is_admin': is_admin, 'pagenow': page, 'pagel0': page-3, 'pagel1': page-2, 'pagel2': page-1, 'pager1': pager1, 'pager2': pager2, 'pager3': pager3, 'is_admin': is_admin})
+	return newrender('title_probs', '', 'probs.html', 'probs', {'submit_list':submit_list, 'key_list':key_list, 'is_admin': is_admin, 'pagenow': page, 'pagel0': page-3, 'pagel1': page-2, 'pagel2': page-1, 'pager1': pager1, 'pager2': pager2, 'pager3': pager3, 'is_admin': is_admin})
